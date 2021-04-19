b85d69220c11f7f78a90c6300831f4a2ac12063b
Neil Daly <neildaly2142@gmail.com>
2021-04-19 21:31:07 +0100

    feat: :sparkles: first draft home ui

--- App.tsx
+++ App.tsx
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.bar}>
+        <View style={styles.cta}>
+          <Text style={styles.welcome}>Welcome Back, Neil!</Text>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => console.log("create")}
+          >
+            <Text style={styles.buttonText}>Create</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View>
+        <Text style={{ paddingLeft: 20 }}>My Workouts</Text>
+        <View style={styles.workouts}>
+          <View style={styles.workout}>
+            <Text style={{ color: "white" }}>Workout 1</Text>
+          </View>
+          <View style={styles.workout}>
+            <Text style={{ color: "white" }}>Workout 2</Text>
+          </View>
+          <View style={styles.workout}>
+            <Text style={{ color: "white" }}>Workout 3</Text>
+          </View>
+          <View style={styles.workout}>
+            <Text style={{ color: "white" }}>Workout 4</Text>
+          </View>
+        </View>
+      </View>
+      <View style={[styles.bar, styles.bottomNav]}>
+        <Text style={styles.welcome}>App Navigation</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +43,56 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "space-between",
+    height: "100%",
+  },
+  bar: {
+    backgroundColor: "#574B90",
+    height: "25%",
+    width: "100%",
+    display: "flex",
+    padding: 20,
+    justifyContent: "space-evenly",
+    alignItems: "flex-start",
+  },
+  cta: {
+    justifyContent: "space-between",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#FC85AE",
+    padding: 10,
+    width: 100,
+    top: 15,
+    borderRadius: 10,
+  },
+  workouts: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "flex-start",
+    padding: 20,
+  },
+  workout: {
+    width: 200,
+    height: 200,
+    padding: 10,
+    marginRight: 10,
+    backgroundColor: "#574B90",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontSize: 20,
+    color: "#000",
+  },
+  bottomNav: {
+    height: "10%",
+    backgroundColor: "#303A52",
+  },
+  welcome: {
+    fontSize: 25,
+    color: "white",
+    fontWeight: "bold",
   },
 });
